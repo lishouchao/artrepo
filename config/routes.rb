516750241108b8930e2b685aa311e016b74a2883
lishouchao <lishouchao@163.com>
2019-01-23 22:18:14 +0800

    Add hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :arts
+
+  root 'application#hello'
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
